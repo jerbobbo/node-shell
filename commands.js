f5cbc616f71c89140a5a696a39832f1ab204a7e5
sishar <sahil.ishar@gmail.com>
2016-01-27 01:40:00 +0000

    implemented done() function

--- commands.js
+++ commands.js
@@ -1,36 +1,36 @@
 var fs = require('fs');
-  
+var request = require('request');
+
+
 function pwd(file) {
-   process.stdout.write(process.cwd());
-   process.stdout.write('\nprompt > ');
+   done(process.cwd());
 }
    
 function date(file) {
     var currDate = new Date().toString();
-    process.stdout.write(currDate);
-    process.stdout.write('\nprompt > ');
+    done(currDate);
 }     
 
 function ls(file) {
     fs.readdir('.', function(err, files) {
         if (err) throw err;
+        var list = '';
         files.forEach(function(file) {
-            process.stdout.write(file.toString() + "\n");
+            list += file.toString() + "\n";
         })
-        newPrompt();
+        done(list);
     });
 }
 
 function echo(file) {
-    process.stdout.write(file);
-    process.stdout.write('\nprompt > ');
+    done(file);
 }
 
 function cat(file) {
     fs.readFile(file, function(error, data) {
         if (error) throw error;
-        process.stdout.write(data.toString());
-        newPrompt();
+        done(data.toString());
+       
     });
 }
 
@@ -38,32 +38,39 @@ function head(file) {
     fs.readFile(file, function(error, data) {
         if (error) throw error;
         var lines = data.toString().split('\n');
+        var headOfFile = '';
         for (var i = 0; i < 5; i++)
-            process.stdout.write(lines[i] + '\n');
-        newPrompt();
+            headOfFile += lines[i] + '\n';
+        done(headOfFile);
     });
 }
 
 function tail(file) {
     fs.readFile(file, function(error, data) {
         if (error) throw error;
+        
+        var output = "";
         var lines = data.toString().split("\n");
         for (var i = lines.length-5; i < lines.length; i++) {
-             process.stdout.write(lines[i] + '\n' );
+            output += lines[i] + '\n';
         }
-        newPrompt();
+        
+        done(output);
     });
 }
 
 function sort(file) {
     fs.readFile(file, function(error, data) {
         if (error) throw error;
+        
+        var output = "";
         var lines = data.toString().split("\n");
         lines = lines.sort();
         lines.forEach(function(line) {
-            process.stdout.write(line + '\n');
+            output += line + '\n';
         });
-        newPrompt()
+        
+        done(output);
     });
 }
 
@@ -72,16 +79,38 @@ function wc(file) {
         if (error) throw error;
         
         var lines = data.toString().split('\n');
-        process.stdout.write(lines.length.toString());
-        newPrompt();
+        done(lines.length.toString());
     })
 }
 
 function uniq(file) {
+    fs.readFile(file, function(error, data) {
+        if (error) throw error;
+        
+        var output = "";
+        var lines = data.toString().split('\n');
+        process.stdout.write(lines[0] + '\n');
+        for (var i = 1; i < lines.length; i++) {
+            if (lines[i] !==  lines[i-1]) {
+                output += lines[i] + '\n';
+            }
+        }
+        
+        done(output);
+    });
+}
+
+function curl(url) {
+    request(url, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            done(body.toString());
+        }
+    });
     
 }
 
-function newPrompt() {
+function done(output) {
+    process.stdout.write(output);
     process.stdout.write('\nprompt > ');
 }
 
@@ -95,5 +124,6 @@ module.exports = {
     tail: tail,
     sort: sort,
     wc: wc,
-    uniq
+    uniq: uniq,
+    curl: curl
 };
